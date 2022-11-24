27fbd52a064c8a731e86e5a3cafa9efb1926c383
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-25 00:45:24 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+   return (
+    <div className="App">
+      <img src='./assets/logo.png'className="logo react" alt="React logo" />
+        
+    </div>
+  )
+}
+
+export default App
