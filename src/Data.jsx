98755536c39452a3bcdfc7573007958e0e12a649
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-25 01:58:07 +0300

    coded

---
+++ src/Data.jsx
@@ -0,0 +1,39 @@
+export Default[
+    {
+        id:1,
+        title: "This is our love story",
+        author: "caleb jephuneh",
+        cost: $20,
+        rating: 5.0,
+
+    },
+    {
+        id:2,
+        title: "Be successful in 21 days",
+        author: "john the ripper",
+        cost: $29,
+        rating: 5.0,
+    },
+    {
+        id:3,
+        title: "Make money while Sleeping",
+        author: "David Einstints",
+        cost: $23,
+        rating: 5.0,
+    },
+    {
+        id:4,
+        title: "This is the best way earn",
+        author: "Mbogholi Jones",
+        cost: $55,
+        rating: 5.0,
+    },
+    {
+        id:5,
+        title: "How to earn a degree without studying",
+        author: "Solomon David",
+        cost: $122,
+        rating: 5.0,
+    },
+
+    ]
