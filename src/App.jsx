d1bf4ced4172e8937595ea7662256b40433e3cc7
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-25 00:50:56 +0300

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Book from './Book'
 import './App.css'
 
 function App() {
    return (
     <div className="App">
       <img src='./assets/logo.png'className="logo react" alt="React logo" />
-        
+      <Book />
     </div>
   )
 }
---
+++ src/Book.jsx
@@ -0,0 +1,9 @@
+const Book = () => {
+    return(
+        <div>
+            <img src='./assets/logo.png'className="logo react" alt="React logo" />
+        </div>
+    )
+}
+
+export default Book
