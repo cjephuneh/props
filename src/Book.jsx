b248d9472000f672c68360c36037106633262dc5
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-25 03:52:59 +0300

    code

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,12 +1,15 @@
 const Book = (props) => {
-    console.log
+    const{img, title, author, cost, rating} = props
     
+
     return(
         <div className="book">
-            <img src='./assets/logo.png'className="logo" alt="React logo" />
-            <h1>This is the love story</h1>
-            <h2>Benjamin Mendy</h2>
-            <h3>$400</h3>
+            <img src={img} className="logo" alt="React logo" />
+             <h1>{title}</h1>
+            <h2>{author}</h2>
+            <h3>{cost}</h3>
+            <h4>{rating}</h4>
+
         </div>
     )
 }
