3c6fecf8162a82e21978d5c7228b8e610a533ea6
caleb jephuneh <calebjephunneh3@gmail.com>
2022-11-25 04:33:03 +0300

    event-handler

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,5 +1,8 @@
 const Book = (props) => {
     const{img, title, author, cost, rating} = props
+    const clickHandler = () =>{
+        alert('hello user')
+    }
     
 
     return(
@@ -9,6 +12,7 @@ const Book = (props) => {
             <h2>{author}</h2>
             <h3>{cost}</h3>
             <h4>{rating}</h4>
+            <button type="button" onClick={clickHandler}>reference button</button>
 
         </div>
     )
